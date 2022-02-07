03942fb0e28b6b1918972a36de793ec8c08692e9
Artur Kozłowski <aarturkozlowski@gmail.com>
2022-02-07 19:05:41 +0100

    Part09_11-Added class organism and Herd with all funcionality. All test passed.

--- mooc-java-programming-ii/part09-Part09_11.Herds/src/main/java/MainProgram.java
+++ mooc-java-programming-ii/part09-Part09_11.Herds/src/main/java/MainProgram.java
@@ -3,6 +3,16 @@
 public class MainProgram {
 
     public static void main(String[] args) {
-        //you can test the program here
+    	Organism mamal = new Organism(0, 0);
+    	System.out.println(mamal);
+    	mamal.move(10, 15);
+    	System.out.println(mamal);
+    	Herd herd = new Herd();
+    	herd.addToHerd(new Organism(57, 66));
+    	herd.addToHerd(new Organism(73, 56));
+    	herd.addToHerd(new Organism(46, 52));
+    	herd.addToHerd(new Organism(19, 107));
+    	System.out.println(herd);
     }
 }
+  

f0969800fc94feda41cd7e8c9bb226dc8fd3824b
Artur Kozłowski <aarturkozlowski@gmail.com>
2022-02-07 20:08:02 +0100

    Part09_12_Addec Animal, Dog, Cat and NoiseCapable class. Implement add methods according to exercise documentation. All test passed.

--- mooc-java-programming-ii/part09-Part09_12.Animals/src/main/java/Main.java
+++ mooc-java-programming-ii/part09-Part09_12.Animals/src/main/java/Main.java
@@ -2,8 +2,26 @@
 public class Main {
 
     public static void main(String[] args) {
-        // you can test how your classes work here
+       Dog dog = new Dog();
+       dog.bark();
+       dog.eat();
+       
+       Dog fido = new Dog("Fido");
+       fido.bark();
+       
+       Cat cat = new Cat();
+       cat.purr();
+       cat.eat();
 
+       Cat garfield = new Cat("Garfield");
+       garfield.purr();
+       NoiseCapable dog1 = new Dog();
+       dog1.makeNoise();
+
+       NoiseCapable cat1 = new Cat("Garfield");
+       cat1.makeNoise();
+       Cat c = (Cat) cat1;
+       c.purr();
     }
 
 }

298f53c877e78ea541fa49752a7c72162cb96b09
grudevart <aarturkozlowski@gmail.com>
2022-02-08 17:17:42 +0100

    Part10_06-Implement program that returns numbers beetwen 1 and 5 from user inputs. All test passed.

--- mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,11 +1,23 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Integer> inputs = new ArrayList<>();
+        
+        while(true) {
+        	int input = Integer.valueOf(scanner.nextLine());
+        	if(input<0) {
+        		break;
+        	}
+        	inputs.add(input);
+        }
+        
+        inputs.stream().filter(number -> number>0 && number<6).forEach(number -> System.out.println(number));
 
     }
 }

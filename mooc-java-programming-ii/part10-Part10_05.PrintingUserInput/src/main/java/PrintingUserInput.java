b300ed6823f3a89db9dcee56977282ac05927a23
grudevart <aarturkozlowski@gmail.com>
2022-02-08 17:03:50 +0100

    Part10_05-Implemet program for printing user string input witch forEach operation. All test passed.

--- mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -1,11 +1,24 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        List<String> inputs = new ArrayList<>();
+        
+        while(true) {
+        	String input = scanner.nextLine();
+        	if(input.isEmpty()) {
+        		break;
+        	}
+        	inputs.add(input);
+        }
+        
+        inputs.stream().forEach(inputString -> System.out.println(inputString));
+        scanner.close();
     }
 }

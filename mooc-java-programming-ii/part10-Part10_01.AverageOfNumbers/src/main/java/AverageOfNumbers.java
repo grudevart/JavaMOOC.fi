1cefd3b98af67ba8519245e9c65dcbcc2e3b991f
Artur Kozłowski <aarturkozlowski@gmail.com>
2022-02-08 11:17:15 +0100

    Part10_01-Frist program using streams, all test passed.

--- mooc-java-programming-ii/part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ mooc-java-programming-ii/part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -1,12 +1,30 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AverageOfNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // Write your program here
+        Scanner scan = new Scanner(System.in);
+        List<String> inputs = new ArrayList<>();
+        System.out.println("Input numbers, type \"end\" to stop.");
+        
+        while(true) {
+        	String input = scan.nextLine();
+        	if(input.equals("end")) {
+        		break;
+        	}
+        	inputs.add(input);
+        	
+        }
+        
+        double average = inputs.stream()
+        .mapToInt(s -> Integer.valueOf(s))
+        .average()
+        .getAsDouble();
+        
+       System.out.println("Average of the numbers: " + average);
 
     }
 }

73987e9404b8b750a7a92ab3b1638195b1ac96e5
grudevart <aarturkozlowski@gmail.com>
2022-02-08 13:12:49 +0100

    Part10_02-Implement second program with use of streams. Wich ask user for input numbers, adn then ask userf for average of positive or negative numbers. handle case when no negative or positive number is inputted. All test passed

--- mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -1,12 +1,34 @@
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class AverageOfSelectedNumbers {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		List<String> inputs = new ArrayList<>();
 
-    }
+		while (true) {
+			System.out.println("Input numbers, type \"end\" to stop");
+			String input = scan.nextLine();
+			if (input.equals("end")) {
+				break;
+			}
+			inputs.add(input);
+		}
+		System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+		String inputChoice = scan.nextLine();
+		if (inputChoice.equals("n")) {
+			double negativeAverage = inputs.stream().mapToInt(s -> Integer.valueOf(s)).filter(number -> number < 0)
+					.average().orElse(0.0);
+			System.out.println(negativeAverage);
+		} else if (inputChoice.equals("p")) {
+			double positiveAverage = inputs.stream().mapToInt(s -> Integer.valueOf(s)).filter(number -> number > 0)
+					.average().orElse(0.0);
+			System.out.println(positiveAverage);
+		}
+		scan.close();
+	}
 }

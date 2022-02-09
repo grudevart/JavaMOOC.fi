a81101d9d15e215a0d3eddb2952ab41aa31f475a
grudevart <aarturkozlowski@gmail.com>
2022-02-09 13:43:47 +0100

    Part10_13-Implement procedure to read csv file with UNESCO literacy level, sort by level of literacy and print back. All test passed.

--- mooc-java-programming-ii/part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ mooc-java-programming-ii/part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -3,10 +3,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class LiteracyComparison {
-    
-    public static void main(String[] args) {
 
-    }
+	public static void main(String[] args) {
+		List<String> rows = new ArrayList<>();
+		try {
+			Files.lines(Paths.get("literacy.csv")).forEach(row -> rows.add(row));
+			;
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		rows.stream().map(row -> row.split(",")).sorted((p1, p2) -> p1[5].compareTo(p2[5])).forEach(
+				p -> System.out.println(p[3] + " (" + p[4] + "), " + p[2].replace("(%)", "").trim() + ", " + p[5]));
+	}
 }

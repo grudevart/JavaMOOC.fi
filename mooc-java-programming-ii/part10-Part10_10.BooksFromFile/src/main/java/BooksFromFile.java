94ff49bd9f16dac080e62ecde8c991cb949e7cb4
grudevart <aarturkozlowski@gmail.com>
2022-02-09 10:26:13 +0100

    Part10_10-remove file.txt

--- mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -4,13 +4,12 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class BooksFromFile {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String file = "A,1,2,D,";
+        String file = "A,1,2,D";
         readBooks(file);
     }
     

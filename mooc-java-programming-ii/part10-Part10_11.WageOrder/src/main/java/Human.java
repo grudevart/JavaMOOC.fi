6821ef23bf78d443dabd8d78803e43a1e6cfd418
grudevart <aarturkozlowski@gmail.com>
2022-02-09 11:15:54 +0100

    Part10_11-Override compareTo in Human class, that now method returning human wage from highest to lowets. All test passed.

--- mooc-java-programming-ii/part10-Part10_11.WageOrder/src/main/java/Human.java
+++ mooc-java-programming-ii/part10-Part10_11.WageOrder/src/main/java/Human.java
@@ -1,5 +1,5 @@
 
-public class Human {
+public class Human implements Comparable<Human> {
 
     private int wage;
     private String name;
@@ -22,4 +22,9 @@ public class Human {
     public String toString() {
         return name + " " + wage;
     }
+
+	@Override
+	public int compareTo(Human human) {
+	return human.wage - this.wage;
+	}
 }

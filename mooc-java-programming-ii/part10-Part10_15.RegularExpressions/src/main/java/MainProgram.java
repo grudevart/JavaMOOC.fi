38bf0b5418ddd0b902bafb0d42957c541e43548d
grudevart <aarturkozlowski@gmail.com>
2022-02-09 19:06:46 +0100

    Part10_15-Implement methods in Checker class, first use of regular expressions. All test passed.

--- mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -1,9 +1,24 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+    	List<String> test = new ArrayList<>();
+    	test.add("tue");
+    	test.add("io");
+    	test.add("queue");
+    	test.add("11:25:00");
+    	Checker regex = new Checker();
+    	for(String string : test) {
+    	if(regex.allVowels(string)) {
+    		System.out.println("ok");
+    	} else {
+    		System.out.println("no ok");
+    	}
+    	}
+    	
     }
 }

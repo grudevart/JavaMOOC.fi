13391c24122279cfb9fcd047b0e308231c4fbd4a
grudevart <aarturkozlowski@gmail.com>
2022-02-09 14:02:23 +0100

    Fetched files

--- mooc-java-programming-ii/part10-Part10_14.Literature/src/main/java/Book.java
+++ mooc-java-programming-ii/part10-Part10_14.Literature/src/main/java/Book.java
@@ -1,4 +1,20 @@
 
 public class Book {
+	
+	private String name;
+	private int ageRecommmendation;
+	
+	public Book(String name, int ageRecommendation) {
+		this.name = name;
+		this.ageRecommmendation = ageRecommendation;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAgeRecommmendation() {
+		return ageRecommmendation;
+	}
 
 }

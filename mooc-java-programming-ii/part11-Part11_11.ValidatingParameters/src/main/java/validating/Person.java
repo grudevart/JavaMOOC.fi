4414a922d3dadcd27897d7b51274f194d6340690
grudevart <aarturkozlowski@gmail.com>
2022-02-13 23:36:48 +0100

    Part11_10-11-Adding Exeptions according to exercse description. All test passed.

--- mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
+++ mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -2,20 +2,23 @@ package validating;
 
 public class Person {
 
-    private String name;
-    private int age;
+	private String name;
+	private int age;
 
-    public Person(String name, int age) {
+	public Person(String name, int age) {
+		if (name==null||name.length() > 40 || name.isEmpty() || age>120||age<0) {
+			throw new IllegalArgumentException();
+		}
 
-        this.name = name;
-        this.age = age;
-    }
+		this.name = name;
+		this.age = age;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	public int getAge() {
+		return age;
+	}
 }
---
+++ mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/main.java
@@ -0,0 +1,10 @@
+package validating;
+
+public class main {
+
+	public static void main(String[] args) {
+		Person p = new Person(null, 1221);
+
+	}
+
+}

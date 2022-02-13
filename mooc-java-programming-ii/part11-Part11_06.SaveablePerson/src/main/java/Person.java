0dd9f25a84f1ee58bff57210c24303080266867c
grudevart <aarturkozlowski@gmail.com>
2022-02-13 13:49:54 +0100

    Part10_05-07-Implement simple classes according to UML class diagrams. All test passed.

---
+++ mooc-java-programming-ii/part11-Part11_06.SaveablePerson/src/main/java/Person.java
@@ -0,0 +1,25 @@
+
+public class Person implements Saveable {
+	private String name;
+	private String address;
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void load(String adress) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}

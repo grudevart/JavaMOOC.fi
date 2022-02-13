fa0cbc4be03f767e72b3fb2e8ecdd33d0aaf8ee5
grudevart <aarturkozlowski@gmail.com>
2022-02-13 20:46:34 +0100

    Part11_10-Copy of the solution provided erlier.

--- mooc-java-programming-ii/part11-Part11_10.FlightControl/src/main/java/FlightControl/Main.java
+++ mooc-java-programming-ii/part11-Part11_10.FlightControl/src/main/java/FlightControl/Main.java
@@ -1,9 +1,18 @@
 package FlightControl;
 
+import java.awt.TextArea;
+import java.util.Scanner;
+
+import FlightControl.logic.FlightControl;
+import FlightControl.ui.TextUI;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Write the main program here. It is useful to create some classes of your own.
+        Scanner scanner = new Scanner(System.in);
+        FlightControl fC = new FlightControl();
+        TextUI ui = new TextUI(fC, scanner);
+        ui.start();
+        
     }
 }

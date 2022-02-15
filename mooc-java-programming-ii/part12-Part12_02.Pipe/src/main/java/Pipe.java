203b56ef3bbd8479f092d2764a7e9ba4f2c2d80a
grudevart <aarturkozlowski@gmail.com>
2022-02-15 13:56:41 +0100

    Part12_02-Added Pipe class with generic implementation of a list. All test passed.

---
+++ mooc-java-programming-ii/part12-Part12_02.Pipe/src/main/java/Pipe.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class Pipe<T> {
+	
+	private ArrayList<T> pipeStack;
+	
+	public Pipe() {
+		pipeStack = new ArrayList<>();
+	}
+	
+	public void putIntoPipe(T value) {
+		pipeStack.add(value);
+	}
+	
+	public T takeFromPipe() {
+		T copy;
+		if(!pipeStack.isEmpty()) {
+			copy = pipeStack.get(0);
+			pipeStack.remove(0);
+			return copy;
+		}
+			return null;
+	}
+	
+	public boolean isInPipe() {
+		 return (!pipeStack.isEmpty());
+	}
+	
+
+}

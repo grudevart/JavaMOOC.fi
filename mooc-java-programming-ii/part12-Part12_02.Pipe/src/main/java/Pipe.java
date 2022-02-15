f911f7a76a9a69b28086a0a0369a0a59008eac49
grudevart <aarturkozlowski@gmail.com>
2022-02-15 14:07:34 +0100

    git

--- mooc-java-programming-ii/part12-Part12_02.Pipe/src/main/java/Pipe.java
+++ mooc-java-programming-ii/part12-Part12_02.Pipe/src/main/java/Pipe.java
@@ -3,30 +3,28 @@ import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class Pipe<T> {
-	
+
 	private ArrayList<T> pipeStack;
-	
+
 	public Pipe() {
 		pipeStack = new ArrayList<>();
 	}
-	
+
 	public void putIntoPipe(T value) {
 		pipeStack.add(value);
 	}
-	
+
 	public T takeFromPipe() {
 		T copy;
-		if(!pipeStack.isEmpty()) {
+		if (!pipeStack.isEmpty()) {
 			copy = pipeStack.get(0);
 			pipeStack.remove(0);
 			return copy;
 		}
-			return null;
+		return null;
 	}
-	
-	public boolean isInPipe() {
-		 return (!pipeStack.isEmpty());
-	}
-	
 
+	public boolean isInPipe() {
+		return (!pipeStack.isEmpty());
+	}
 }
